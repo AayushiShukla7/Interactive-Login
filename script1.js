e038a9763a099bb6573a75d7afddc7aeb445e9d4
Aayushi Shukla <AayushiShukla27@gmail.com>
2025-02-20 20:38:42 -0500

    Spline background added

--- script1.js
+++ script1.js
@@ -1,3 +1,13 @@
+// Loading Spinner
+var loadingScreen = document.querySelector(".loadingScreen");
+
+// Fake the loading - for aesthetics**
+window.addEventListener('load', function() {
+    this.setTimeout(async function() {
+        loadingScreen.style.display = 'none';
+    }, 3000);
+});
+
 const container = document.querySelector('.container');
 const registerBtn = document.querySelector('.register-btn');
 const loginBtn = document.querySelector('.login-btn');
@@ -9,3 +19,7 @@ registerBtn.addEventListener('click', () => {
 loginBtn.addEventListener('click', () => {
     container.classList.remove('active');
 });
+
+// Spline - Switch between normal and angry faces
+var normalFace = document.getElementById("normal-face");
+var angryFace = document.getElementById("angry-face");
