60a811aa43ed1a995590ea026ac16ae52a5dd0c7
Aayushi Shukla <AayushiShukla27@gmail.com>
2025-02-20 21:18:04 -0500

    restructure and background update

--- script1.js
+++ script1.js
@@ -12,6 +12,10 @@ const container = document.querySelector('.container');
 const registerBtn = document.querySelector('.register-btn');
 const loginBtn = document.querySelector('.login-btn');
 
+// Form Buttons
+const loginButton = document.getElementById("loginButton");
+const registerButton = document.getElementById("registerButton");
+
 registerBtn.addEventListener('click', () => {
     container.classList.add('active');
 });
@@ -21,5 +25,41 @@ loginBtn.addEventListener('click', () => {
 });
 
 // Spline - Switch between normal and angry faces
-var normalFace = document.getElementById("normal-face");
-var angryFace = document.getElementById("angry-face");
+const normalFace = document.getElementById("normal-face");
+const angryFace = document.getElementById("angry-face");
+
+// Validate
+function validateForm() {
+    let username = document.forms["myLoginForm"]["username"].value;
+    let password = document.forms["myLoginForm"]["password"].value;
+    
+    //let email = document.forms["myRegistrationForm"]["email"].value;
+
+    if (username == "" || password == "") {
+        //alert("Name & Password must be filled out!");
+
+        normalFace.style.display = 'none';
+        angryFace.style.display = 'block';
+
+        // if(normalFace.style.display === "none"){
+        //     angryFace.style.display = 'none';
+        //     normalFace.style.display = 'block';
+        // }
+        // else{
+        //     normalFace.style.display = 'none';
+        //     angryFace.style.display = 'block';
+        // }
+
+        // Set focus on the empty field
+        if(username == "") 
+            document.forms["myLoginForm"]["username"].focus();
+        else
+            document.forms["myLoginForm"]["password"].focus();
+
+        return false;
+    }
+    else {
+        angryFace.style.display = 'none';
+        normalFace.style.display = 'block';
+    }
+}
